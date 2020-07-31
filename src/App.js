da0b65397f023669f8993a0f29fb10406b26abd1
Erick Silva <erick.ap.silva@gmail.com>
2020-07-31 08:36:45 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StylesProvider } from '@material-ui/styles';
+import { CssBaseline } from '@material-ui/core';
+import GlobalStyles  from './commons/styles/global-style';
+import Main from './containers/Main';
+
+function App() {
+  return (
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <GlobalStyles />
+      <Main />
+    </StylesProvider>
+  );
+}
+
+export default App;
